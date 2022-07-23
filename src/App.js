531c25cbedfd13919c46dcae699afbd3684aabb4
sheraz064-ai <sherazsiddiqui0456@gmail.com>
2022-07-23 11:00:55 +0500

    automatic batching example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [additionCount, setAdditionCount] = useState(0);
+  const [subtractionCount, setSubtractionCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  console.log("Component Rendering");
+
+  // const handleOnClick = () => {
+  //   setAdditionCount(additionCount + 1);
+  //   setSubtractionCount(subtractionCount - 1);
+  // };
+
+  const handleOnClickAsync = async () => {
+    setLoading(true);
+    console.log("set loading");
+    await fetch("https://jsonplaceholder.typicode.com/todos/1").then(() => {
+      setAdditionCount(additionCount + 1);
+      setSubtractionCount(subtractionCount - 1);
+      console.log("callback fetch");
+    });
+    setLoading(false);
+    console.log("unset loading");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        style={{ width: "50%", height: "30%" }}
+        onClick={() => {
+          // handleOnClick();
+          handleOnClickAsync();
+        }}
+      >
+        Click Me!
+      </button>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {" "}
+          <div>Add Count: {additionCount}</div>﻿
+          <div>Substraction Count: {subtractionCount}</div>
+        </>
+      )}
     </div>
   );
 }
